83ed5bf99e20da326b85960deae41fe8623fff11
Mohammed Adam <adamekissa@gmail.com>
2024-06-13 15:05:59 +0100

    movie list added

--- src/MapPractice2.jsx
+++ src/MapPractice2.jsx
@@ -3,12 +3,24 @@ export default function MapPractice2() {
     const [movies, regenerate] = useMovieGenerator();
     console.log("movies", movies);
 
+    const showMovies = (movie => {
+        return (
+            <div className="movie">
+                <h2 className="title">{movie.title}</h2>
+                <p>Starring: {movie.star}</p>
+                <p>Genre: {movie.genre}</p>
+                <blockquote>Iconic quote: {movie.quote}</blockquote>
+            </div>   
+        )
+    }
+    )
+
     return (
         <div className="mapPractice">
-            <h2>Map Practice 2</h2>
+            <h2>Movie list</h2>
             <button onClick={regenerate}>Change Movies</button>
             <div className="movieList">
-                This is where your movies need to go! (See expected2.html)
+                {movies.map(showMovies)}
             </div>
         </div>
     );
